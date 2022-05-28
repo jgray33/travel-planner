2c92897ec12dc671366ab9fab740cf95540df98c
Julia <julia.gray30@gmail.com>
2022-05-28 13:53:37 +0100

    Type defs for trip added and resolve for adding a new trip

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,34 @@
-const { User } = require('../models')
+const { User } = require("../models");
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find()
-        }
-    }
-}
+  Query: {
+    // Find all users
+    users: async () => {
+      return User.find();
+    },
+    // Find all trips
+    trips: async () => {
+      return Trip.find();
+    },
+    // Find one trip
+    trip: async (parent, { tripId }) => {
+      return Trip.findOne({ _id: tripId });
+    },
+  },
 
-module.exports = resolvers
+  Mutation: {
+    //   Add a new user
+    addUser: async (parent, { username, email, password }) => {
+      return User.create({ username, email, password });
+    },
+    // Add a new trip
+    addTrip: async (
+      parent,
+      { tripName, description, location, startDate, endDate }
+    ) => {
+      return Trip.create({ tripName, description, location, startDate, endDate });
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,10 +6,28 @@ type User {
     username: String!
     email: String!
     password: String!
+    trips: [Trip]
 }
+type Trip {
+    _id: ID
+    tripName: String
+    description: String
+    location: [Float] 
+    startDate: String
+    endDate: String
+    plans: [Plan]
+    facts: [Fact]
+    }
 
 type Query {
     users: [User]!
+    trips: [Trip]
+    trip(tripId: ID!): Trip
+}
+
+type Mutation {
+    addUser(userId: ID!, username:String!, email:String!, password: String!): User
+    addTrip(tripId: ID!, tripName: String, description: String, location: [Float], startDate: String, endDate: String): Trip
 }
 `
 
