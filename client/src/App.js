2c61311ce76c537de659e62529a5ab7b1c71dc36
Julia <julia.gray30@gmail.com>
2022-06-09 20:56:15 +0100

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,39 @@
 import React from 'react'
 import {
-  ApolloClient,
-  InMemoryCache,
+  ApolloClient, 
+  InMemoryCache, 
   ApolloProvider, 
-  createHttpLink, 
+  createHttpLink
 } from '@apollo/client'
-
-import { setContext } from '@apollo/client/link/context'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { setContext } from '@apollo/client/link/context';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const authLink = setContext((_, {headers}) => {
-  const token = localStorage.getItem('id_token')
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
   return {
     headers: {
-      ...headers, 
-      authorization: token? `Bearer ${token}` : '',
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
     },
-  }
-})
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
     <div className="App">
-     <h1>Hello</h1>
+     <h1>Hi</h1>
     </div>
-    </Router>
     </ApolloProvider>
   );
 }
