aad1c4b7e002edc9167421cab7aa2d06af81a15b
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-11 23:35:16 +0100

    deploying

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/", (req, res) => {
