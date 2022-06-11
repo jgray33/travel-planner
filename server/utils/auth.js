0896bb7754b8cb49f1922abb69aec83ae4f48df2
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-11 13:33:56 +0100

    Fixed login and signup

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,31 +1,31 @@
-require ("dotenv").config()
+require("dotenv").config();
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const secret = process.env.AUTH_SECRET;
-console.log(secret)
-const expiration = '2h';
+console.log(process.env.AUTH_SECRET);
+const expiration = "2h";
 
 module.exports = {
-authMiddleware: function ({req}) {
-    let token = req.body.token || req.query.token || req.headers.authorization
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
-        token = token.split('').pop().trim()
+      token = token.split("").pop().trim();
     }
     if (!token) {
-    return req
-}
+      return req;
+    }
 
-try {
-    const {data} = jwt.verify(token, secret, {maxAge: expiration})
-    req.user = data
-} catch {
-    console.log("Invalid token")
-}
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
 
-return req
-},
+    return req;
+  },
 
   signToken: function ({ email, name, _id }) {
     const payload = { email, name, _id };
