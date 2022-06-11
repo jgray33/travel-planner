f19f5f863a1a298455282ebdedb6cf6ab0e43c20
Julia <julia.gray30@gmail.com>
2022-06-11 12:06:24 +0100

    login and signup working

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from './pages/Home'
 import Trip from "./pages/TripPage";
+import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -38,6 +39,7 @@ function App() {
         <div>
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/trip" element={<Trip />} />
             <Route path="/" element={<Home />} />
             </Routes>
