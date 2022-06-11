5fab1dacc5fb531edd5d376ee6ee2c7aab69f622
Andrea Mac <aleatrengrouse@gmail.com>
2022-06-11 13:28:54 +0100

    installed react bootstrap and create files for cards

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 // import "./card.css";
 // import Card from "react-bootstrap/Card";
-import { Card as ReactCard } from "react-bootstrap"
+import { Card as ReactCard } from "react-bootstrap";
 
 const Card = () => {
   return (
     <div className="c">
       <div className="c-browser">
-        <ReactCard
+        <ReactCard />
         <div className="c-circle"></div>
         <div className="c-circle"></div>
         <div className="c-circle"></div>
