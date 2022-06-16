a704cf21886e018353415c63a2d1e5304f39fc94
Andrea Mac <aleatrengrouse@gmail.com>
2022-06-16 20:46:07 +0100

    pushing style theme again

--- client/src/components/EditFactButton.js
+++ client/src/components/EditFactButton.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { useMutation } from '@apollo/client'
-import { UPDATE_FACT } from '../utils/mutations'
+import React from "react";
+import { useMutation } from "@apollo/client";
+import { UPDATE_FACT } from "../utils/mutations";
 
-const EditFactButton = ({formState, factId}) => {
-    const [editFact, {error}] = useMutation(UPDATE_FACT)
+const EditFactButton = ({ formState, factId }) => {
+  const [editFact, { error }] = useMutation(UPDATE_FACT);
 
-    const handleFormSubmit = async (event) => {
-        try {
-            const {data} = editFact({
-                variables: {
-                    factId: factId,
-                    description: formState.description
-                }
-            })
-        } catch (err) {
-            console.log(err)
-        }
+  const handleFormSubmit = async (event) => {
+    try {
+      const { data } = editFact({
+        variables: {
+          factId: factId,
+          description: formState.description,
+        },
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return (
-        <button
-        className="btn btn-primary btn-block py-3"
-        onClick={handleFormSubmit}
-      >
-        Update
-      </button>
-    )
-}
+  return (
+    <button
+      className="btn btn-primary btn-block py-3"
+      onClick={handleFormSubmit}
+    >
+      Update
+    </button>
+  );
+};
 
-export default EditFactButton
+export default EditFactButton;
