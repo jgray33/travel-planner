c0b7fc91490bd5dd53854491947dd70a11d69b25
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-16 20:00:49 +0100

    Fixed links and server

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,33 +10,33 @@ const Header = () => {
   };
   return (
     <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a
-        href="/"
+      <Link
         className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+        to={`/`}
       >
         <h1 className="bi m-3 fs-4">Travel Planner</h1>
-      </a>
+      </Link>
 
       <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li>
-          <a href="/" className="nav-link px-2 link-secondary">
+          <Link className="nav-link px-2 link-secondary" to={`/`}>
             Home
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/about" className="nav-link px-2 link-secondary">
+          <Link className="nav-link px-2 link-secondary" to={`/about`}>
             About
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/contact" className="nav-link px-2 link-secondary">
+          <Link className="nav-link px-2 link-secondary" to={`/contact`}>
             Contact
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/donate" className="nav-link px-2 link-secondary">
+          <Link className="nav-link px-2 link-secondary" to={`/donate`}>
             Donate
-          </a>
+          </Link>
         </li>
       </ul>
 
--- server/server.js
+++ server/server.js
@@ -22,13 +22,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // Fix Heroku react routing
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
