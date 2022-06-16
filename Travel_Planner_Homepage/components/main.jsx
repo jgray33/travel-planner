a4ebeb569c28cde1de691487b4f2abd3bac44c6c
DBAX7 <94017236+DBAX7@users.noreply.github.com>
2022-06-16 13:49:37 +0100

    trying to add overlay

--- Travel_Planner_Homepage/components/main.jsx
+++ Travel_Planner_Homepage/components/main.jsx
@@ -3,7 +3,12 @@ import video from '../assets/Travel_Planner_Background_Compressed'
 
 const Main= () => {
     <div className='main'> 
+    <div className='overlay'></div>
     <video src={video} autoPlay loop muted/>
+    <div className='content'>
+        <h1> Welcome </h1> 
+        <p> To Travel Planner</p>
+    </div>
 
     </div>
 }
