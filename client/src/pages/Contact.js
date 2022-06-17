6a6530085cc52a27680f41360e980cae8cffd86a
Corey96 <0coreyy@gmail.com>
2022-06-17 23:09:56 +0100

    created first profile with links

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from './logo.png';
+import logo from './group.png';
 
 const Contact = () => {
   return (
@@ -13,13 +13,25 @@ const Contact = () => {
   );
 };
 
-
-
 function Header() {
   // Import result is the URL of your image
   return <img src={logo} alt="Logo" />;
 }
 
-export default Header;
+const Profiles = () => {
+  return (
+    <div className='container mt-2'>
+      <h2 className='display-6'>Julia</h2>
+      <Link to={'https://github.com/jgray33'} className='nav-link px-2 link-dark underline-on-hover'>
+      Julia Github
+      </Link>
+    </div>
+  );
+};
 
+
+
+
+
+export default Header;
 export default Contact;
