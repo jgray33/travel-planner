416721ea662c7ee7312ca8f70141b317ea783c5b
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-17 11:19:34 +0100

    Added top border to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 
 const Footer = () => {
   return (
-    <footer className="mb-2 mt-auto">
+    <footer className="mb-2 mt-auto border-top pt-2">
       <NavBar />
       <p className="text-center text-muted">&copy; 2022 Travel Planner</p>
     </footer>
