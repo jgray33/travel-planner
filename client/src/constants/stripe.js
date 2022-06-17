d9628bf3a3112cc4f739c814b957703160093872
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-06-17 19:36:48 +0100

    Not working yet

--- client/src/constants/stripe.js
+++ client/src/constants/stripe.js
@@ -1,6 +1,6 @@
 const STRIPE_PUBLISHABLE =
   process.env.NODE_ENV === "production"
-    ? "pk_live_MY_PUBLISHABLE_KEY"
+    ? "pk_test_51LAZQTDfdJmsnTdjAgUjygsk7MiHR87RcjuPfkR5DnPU2pWLtRo9rtO4CuyqjWG37h60FGnpaVhKqW0bWpOYz3Tf00PrDjzjim"
     : "pk_test_51LAZQTDfdJmsnTdjAgUjygsk7MiHR87RcjuPfkR5DnPU2pWLtRo9rtO4CuyqjWG37h60FGnpaVhKqW0bWpOYz3Tf00PrDjzjim";
 
 export default STRIPE_PUBLISHABLE;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ require("dotenv").config();
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
+// Stripe Setup
+const SERVER_CONFIGS = require("./constants/server");
+const configureServer = require("./server-config");
+const configureRoutes = require("./stripe-routes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -22,6 +27,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+configureServer(app);
+configureRoutes(app);
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -32,8 +40,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
   });
 
   db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
+    app.listen(SERVER_CONFIGS.PORT, () => {
+      console.log(`API server running on port ${SERVER_CONFIGS.PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
       );
